6f9f0f303cf446453ac691328b655edd6bbf5c78
hirasawayui267 <92097440+hirasawayui267@users.noreply.github.com>
2022-02-14 19:59:25 -0500

    new commit

---
+++ 9f0279dd-30eb-4207-baf4-675538d01155.py
@@ -0,0 +1,7 @@
+# create your main function here
+# run the necessary operaitons and return
+import json
+def entropy_main(event, context):
+    # the input event is in a json string
+    # event = json.loads(event)
+    return {"response": "hellow orld from joy" }
